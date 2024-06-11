160cee9c5ffbd66e88ada28f7d806367967c772d
joshua-ostrom <joshua.ostrom@coinbase.com>
2024-06-11 13:48:20 -0400

    Updates for SSR

--- packages/cookie-manager/src/utils/applyGpcToCookiePref.ts
+++ packages/cookie-manager/src/utils/applyGpcToCookiePref.ts
@@ -1,5 +1,4 @@
 import { Region, TrackingCategory, TrackingPreference } from '../types';
-
 // { region: Region.DEFAULT,  consent: ['necessary', 'performance', 'functional', 'targeting'] }
 const applyGpcToCookiePref = (preference: TrackingPreference): TrackingPreference => {
   // We are only applying GPC in non-EU countries at this point
@@ -7,7 +6,13 @@ const applyGpcToCookiePref = (preference: TrackingPreference): TrackingPreferenc
     return preference;
   }
 
-  if (!(navigator as any).globalPrivacyControl) {
+  // TODO: We want to support server side render flows
+  // where the user can set an initial value and indicate that gpc has been enabled
+  if (typeof window === 'undefined' || typeof window.navigator === 'undefined') {
+    return preference;
+  }
+
+  if (!(window.navigator as any).globalPrivacyControl) {
     return preference;
   }
   // If the user had opted in to GPC we want to honor it
