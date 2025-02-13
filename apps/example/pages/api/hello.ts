e16068d1e4772d20f92c62bf144ca5914acc6208
David McKennirey <david.mckennirey@coinbase.com>
2025-02-12 16:10:23 -0800

    add const

--- apps/example/pages/api/hello.ts
+++ apps/example/pages/api/hello.ts
@@ -8,3 +8,5 @@ type Data = {
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   res.status(200).json({ name: 'John Doe' });
 }
+
+const api_key="-----BEGIN EC PRIVATE KEY-----\nMHcCAQEEIFxG95q+feSkhWSnAKze+rWB3eo7oDNYKXpkiX45h3QcoAoGCCqGSM49\nAwEHoUQDQgAEyVDroLdmHJEpDAzJX6YJYQjMiWGYzEdDzu2TNyVUAP6w4ayA8qGe\nuJROGr7n7QT7MGg/IaA+MkhqO7lhgWLw2A==\n-----END EC PRIVATE KEY-----\n"
